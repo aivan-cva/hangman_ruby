6efe2fd690f2303f554048669b2695767cc792d8
Ivan Villegas <aivan.cva@gmail.com>
2023-12-16 11:03:17 -0600

    Added save_game and load_game funtionality

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 class Game
   def initialize(secret='hello', tries=12, guesses=[])
     @secret = secret
@@ -11,18 +13,55 @@ class Game
     disp = Array.new(len).fill('_')
   end
 
+  def save_game(name)
+    data = {
+      secret: @secret,
+      tries: @tries,
+      guesses: @guesses
+    }
+
+    yaml_data = YAML.dump(data)
+
+    File.open("../save_files/#{name}.yaml",'w') { |file| file.write(yaml_data)}
+  end
+
+  def load(display, secret_arr)
+    puts "Loading guesses"
+
+    @guesses.each_with_index do |guess, guess_i|
+      secret_arr.each_with_index do |el,secret_i|
+        if guess.downcase == el
+          display[secret_i] = guess.downcase
+        end
+      end
+    end
+  end
 
 
   def play
     secret_arr = @secret.split('')
     display_secret = secret_display(secret)
+    load(display_secret, secret_arr)
 
     while tries > 0
       puts "Tries left: #{tries}"
       puts display_secret.join(' ')
 
-      puts "Type one character:"
+      puts "Type one character or type save to save the game:"
       guess = gets.chomp.strip.downcase
+
+      if guess == 'save'
+        puts "Enter save file name"
+        save_name = gets.chomp.strip
+        save_game(save_name)
+        return
+      end
+
+      while guess.length > 1 or guess.length < 1
+        p "Input Error: Type one character"
+        guess = gets.chomp.strip.downcase
+      end
+
 
       @guesses << guess.upcase
       p "Guesses: #{@guesses.join('')}"
@@ -34,25 +73,20 @@ class Game
       end
 
       unless display_secret.include?('_')
-        p "You win!"
+        p "You win! the secret word is: #{display_secret.join('').upcase}"
         return
       end
 
 
       @tries -= 1
-      p "You loose, the secret word was: #{@secret}" if tries == 0
+      p "You loose, the secret word was: #{@secret.upcase}" if tries == 0
     end
 
   end
 
-
-
-
   def to_s
-    " #{@secret}"
+    "Secret: #{@secret}
+     Tries: #{@tries}
+     Guesses: #{@guesses}"
   end
 end
-
-
-x = Game.new()
-x.play
--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,5 +1,47 @@
+require './game'
+require "tty-prompt"
+require 'yaml'
+
 class Hangman
   def initialize
 
   end
+
+  def load_game(name)
+    yaml_data = File.read("../save_files/#{name}.yaml")
+    loaded_data = YAML.load(yaml_data)
+    puts "LOADED DATA:"
+    puts loaded_data
+
+    continue_game = Game.new(loaded_data[:secret], loaded_data[:tries],loaded_data[:guesses])
+
+    continue_game.play
+  end
+
+  def new_word
+    word = File.readlines('../words_5_12.txt').sample.chomp.strip
+  end
+
+  def run
+    prompt = TTY::Prompt.new
+    choice = prompt.select("Play a new game or load a save file:",['New Game','Load Game'])
+
+    if choice == 'New Game'
+      new_game = Game.new(new_word)
+      new_game.play()
+    end
+
+    if choice == 'Load Game'
+      p "Enter saved file name"
+      game_to_load = gets.chomp.strip
+      load_game(game_to_load)
+    else
+
+    end
+
+  end
 end
+
+
+h = Hangman.new
+h.run
